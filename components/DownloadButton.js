104edd978b7c7fbe7ba93a6d218afb16e208d094
Clement Legrand <clement@whentocop.fr>
2022-07-15 08:45:34 +0200

    update guide pdf

--- components/DownloadButton.js
+++ components/DownloadButton.js
@@ -2,7 +2,7 @@ import styles from '../styles/Button.module.css'
 import Link from 'next/link'
 
 const DownloadButton = () => (
-  <a href="/pdf/Guide-utilisation-Genesis.pdf" target="_blank" className={styles.downloadButtoncontainer}>Télécharger</a>
+  <a href="/pdf/Guide-utilisation-Genesis-Backpack.pdf" target="_blank" className={styles.downloadButtoncontainer}>Télécharger</a>
 )
 
 export default DownloadButton
